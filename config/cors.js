6fd82c67abce71e9ad046abeb9feee07acc2d7df
James Talamo <jamestalamo4@gmail.com>
2024-11-25 23:16:42 +0800

    update 1.0.2

--- config/cors.js
+++ config/cors.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 
 const allowedOrigins = process.env.allowedOrigins.split(','); 
 
-console.log(allowedOrigins)
-
 const corsOptions = {
     origin: (origin, callback) => {
         if (!origin || allowedOrigins.includes(origin)) {
